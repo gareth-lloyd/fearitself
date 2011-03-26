d6901502a058a3011561e79325bb588f163035b7
Ben Smith <ben@thesmith.co.uk>
2011-03-26 12:10:46 +0000

    setup populate / process food

--- populate.py
+++ populate.py
@@ -1,6 +1,10 @@
-from googlereader import Reader, getCredentials
-from story_processor import saveStories
-from models import *
+#!/usr/bin/env python
+
+from stories.googlereader import Reader, getCredentials
+from stories.story_processor import saveStories
+from stories.models import *
+from django.core.management import setup_environ
+import settings
 
 STANDARD_URL= "http%3A%2F%2Fwww.thisislondon.co.uk%2Fstandard-home%2Frss%2F%3Fito%3D1588"
 MAIL_URL = "http%3A%2F%2Fwww.dailymail.co.uk%2Fnews%2Findex.rss"
@@ -15,3 +19,7 @@ def populate():
     r = Reader(getCredentials())
     saveStories(r.getFeed(MAIL_URL), mail)
     saveStories(r.getFeed(STANDARD_URL), standard)
+
+if __name__ == "__main__":
+    setup_environ(settings)
+    populate()
