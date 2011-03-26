64523cf38ad16034de54e41895e8bdca72669a01
Ben Smith <ben@thesmith.co.uk>
2011-03-26 15:28:08 +0000

    processing known sources

--- stories/cleaner.py
+++ stories/cleaner.py
@@ -21,11 +21,21 @@ def clean(webStory):
             introcopy = soup.find('p', {'class': 'introcopy'})
             if (introcopy):
                 texts.append(introcopy.renderContents().encode('ascii'))
+        except UnicodeDecodeError:
+            print "bad text in intro "+webStory.link
+        try:
             bodycopy = soup.find('p', {'id': 'bodycopy'})
             if (bodycopy):
                 texts.append(bodycopy.renderContents().encode('ascii'))
         except UnicodeDecodeError:
-            print "bad text in "+webStory.link
+            print "bad text in body "+webStory.link
+        
+        stories = soup.findAll('p', {'class': 'storycopy'})
+        for copy in stories:
+            try:
+                texts.append(copy.renderContents().encode('ascii'))
+            except UnicodeDecodeError:
+                print "bad text in storycopy "+webStory.link
             
     elif (webStory.source.id == standard.id):
         article = soup.find('div', { 'id': 'article'})
@@ -34,7 +44,41 @@ def clean(webStory):
             for p in paragraphs:
                 texts.append(_processPara(p))
                 
+    elif (webStory.source.id == mirror.id):
+        article = soup.find('div', {'class': 'article-body'})
+        if (article):
+            paragraphs = article.findAll('p')
+            for p in paragraphs:
+                texts.append(_processPara(p))
+                
+    elif (webStory.source.id == telegraph.id):
+        article = soup.find('div', {'class': 'mainBodyArea'})
+        if (article):
+            paragraphs = article.findAll('p')
+            for p in paragraphs:
+                texts.append(_processPara(p))
+                
+    elif (webStory.source.id == express.id):
+        paragraphs = soup.findAll('p', {'class': 'storycopy'})
+        for p in paragraphs:
+            texts.append(_processPara(p))
+            
+    elif (webStory.source.id == guardian.id):
+        article = soup.find('div', {'class': 'article-body-blocks'})
+        if (article):
+            paragraphs = article.findAll('p')
+            for p in paragraphs:
+                texts.append(_processPara(p))
+                
+    elif (webStory.source.id == sun.id):
+        article = soup.find('div', {'class': 'bodyText'})
+        if (article):
+            paragraphs = article.findAll('p')
+            for p in paragraphs:
+                texts.append(_processPara(p))
+                
     else:
+        print "Default cleaner for: "+webStory.link
         paras = soup.findAll('p')
         for p in paras:
             texts.append(_processPara(p))
